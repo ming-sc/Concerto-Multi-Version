ec728a0050b09cce995ee92a38a901f388871255
IMG <1973581872@qq.com>
2026-03-02 03:45:45 +0800

    fix: 修复 CHANNEL 错误的初始化顺序

--- forge/forge-1.20.6/src/main/java/top/gregtao/concerto/network/ConcertoNetworking.java
+++ forge/forge-1.20.6/src/main/java/top/gregtao/concerto/network/ConcertoNetworking.java
@@ -15,8 +15,8 @@ public class ConcertoNetworking {
     public static final String VERSION = "1.0.0";
 
     public static final SimpleChannel CHANNEL = ChannelBuilder.named(ResourceLocation.fromNamespaceAndPath(ConcertoClient.MOD_ID, "network"))
-            .optionalServer()
             .acceptedVersions(Channel.VersionTest.exact(1))
+            .optionalServer()
             .networkProtocolVersion(1)
             .simpleChannel();
 }
